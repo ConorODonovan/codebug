5a1a376e363f4e97f764c3646c154a007aa9f4e0
Conor <conor.m.odonovan@gmail.com>
2025-04-20 19:30:55 +0100

    Homepage started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopToolbarComponent } from "./top-toolbar/top-toolbar.component";
+import { HomepageComponent } from "./homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopToolbarComponent],
+  imports: [RouterOutlet, TopToolbarComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TopToolbarComponent } from "../top-toolbar/top-toolbar.component";
+
+@Component({
+  selector: 'app-homepage',
+  imports: [TopToolbarComponent],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
