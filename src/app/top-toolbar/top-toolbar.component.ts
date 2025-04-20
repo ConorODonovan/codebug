1a8a889d8fe7f70ae946ccb79ef1e2ed1b6d01c1
Conor <conor.m.odonovan@gmail.com>
2025-04-20 15:37:50 +0100

    Initial commit

---
+++ src/app/top-toolbar/top-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-toolbar',
+  imports: [],
+  templateUrl: './top-toolbar.component.html',
+  styleUrl: './top-toolbar.component.css'
+})
+export class TopToolbarComponent {
+
+}
